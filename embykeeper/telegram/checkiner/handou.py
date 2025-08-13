ff1060caca2b09a20dc1ce733dbddf8484ff5290
yeezylife <30175425+yeezylife@users.noreply.github.com>
2025-08-13 18:56:52 +0800

    Create handou.py

---
+++ embykeeper/telegram/checkiner/handou.py
@@ -0,0 +1,10 @@
+from . import BotCheckin
+
+__ignore__ = True
+
+
+class HandouCheckin(BotCheckin):
+    name = "憨豆"
+    bot_username = "bean21bot"
+    bot_checkin_cmd = "/signin"
+    bot_checked_keywords = "签到成功"
