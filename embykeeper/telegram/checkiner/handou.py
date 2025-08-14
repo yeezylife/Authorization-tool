a238991ddb420c12ccea0edd99afb5583d0b8738
yeezylife <30175425+yeezylife@users.noreply.github.com>
2025-08-14 12:16:04 +0800

    Update handou.py

--- embykeeper/telegram/checkiner/handou.py
+++ embykeeper/telegram/checkiner/handou.py
@@ -7,5 +7,5 @@ class HandouCheckin(BotCheckin):
     name = "憨豆"
     bot_username = "bean21bot"
     bot_checkin_cmd = "/signin"
-    bot_success_keywords = "签到成功"
-    bot_checked_keywords = "今日已签到"
+    bot_success_keywords = "签到成功，"
+    bot_checked_keywords = "今日已签到，请明日再来"
